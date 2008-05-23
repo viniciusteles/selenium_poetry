2b0a999448dc6f4b7519486709b7094ec7f89df2
Vinicius Manhaes Teles <vinicius@vini-mac.local>
2008-05-23 12:39:49 -0300

    Brief description of each command in the API for now.

--- lib/selenium_poetry.rb
+++ lib/selenium_poetry.rb
@@ -60,78 +60,93 @@ module SeleniumPoetry
     end
   end
   
+  # Is equivalent to Selenium's <tt>assert_element_not_present</tt>. 
+  # 
+  # Take a look at should_have for further explanation.
   def should_not_have(*selector_keys)
     run_protected do
       selector_keys.each { |key| assert_element_not_present @selectors[key] } 
     end
   end
   
+  # Is equivalent to Selenium's <tt>wait_for_element_present</tt>. 
   def should_wait_for(*selector_keys)
     run_protected do
       selector_keys.each { |key| wait_for_element_present @selectors[key] } 
     end
   end
   
+  # Is equivalent to Selenium's <tt>wait_for_element_not_present</tt>. 
   def should_wait_for_absence_of(*selector_keys)
     run_protected do
       selector_keys.each { |key| wait_for_element_not_present @selectors[key] } 
     end
   end
   
+  # Is equivalent to Selenium's <tt>click</tt>. 
   def click_on(*selector_keys)
     run_protected do
       selector_keys.each { |key| click @selectors[key] }
     end
   end
-  
+
+  # Is equivalent to Selenium's <tt>click_and_wait</tt>.  
   def click_on_and_wait(*selector_keys)
     run_protected do
       selector_keys.each { |key| click_and_wait @selectors[key] }
     end
   end
   
+  # Is equivalent to Selenium's <tt>assert_eval</tt>.  
   def should_equal(selector_keys_and_expected_values)
     run_protected do
       selector_keys_and_expected_values.each { |key, expected_value| assert_eval @selectors[key], expected_value }
     end
   end
 
+  # Is equivalent to Selenium's <tt>wait_for_eval</tt>.  
   def should_wait_for_equal(script_expected)
     run_protected do
       script_expected.each { |script, expected| wait_for_eval @selectors[script], expected }
     end
   end
   
+  # Is equivalent to Selenium's <tt>assert_text</tt>.  
   def should_have_text(key, text)
     run_protected do
       assert_text @selectors[key], text
     end
   end
 
+  # Is equivalent to Selenium's <tt>assert_eval somethind, true</tt>.  
   def should_be(*selector_keys)
     run_protected do
       selector_keys.each { |key| assert_eval @selectors[key], true }
     end
   end
-  
+
+  # Is equivalent to Selenium's <tt>assert_eval somethind, false</tt>.  
   def should_not_be(*selector_keys)
     run_protected do
       selector_keys.each { |key| assert_eval @selectors[key], false }
     end
   end
   
+  # Is equivalent to Selenium's <tt>store_eval</tt>.  
   def should_store(script_variable)
     run_protected do 
       script_variable.each { |script, variable| store_eval @selectors[script], variable }
     end
   end
   
+  # Is equivalent to Selenium's <tt>type</tt>.  
   def type_on(selector_keys_and_expected_values)
     run_protected do
       selector_keys_and_expected_values.each { |key, expected_value| type @selectors[key], expected_value }
     end
   end
   
+  # Is equivalent to Selenium's <tt>drag_and_drop_to_object</tt>.  
   def should_drag_and_drop(key_of_origin, key_of_target)
     run_protected do
       drag_and_drop_to_object @selectors[key_of_origin], @selectors[key_of_target]
